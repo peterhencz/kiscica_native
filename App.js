28e58b8dd92d24209414af53675258b485a00b41
Peter Hencz <peter.hencz@gmail.com>
2019-04-11 19:03:40 +0200

    save

--- App.js
+++ App.js
@@ -11,7 +11,25 @@ const AppNavigator = createBottomTabNavigator(
     Play: HomeScreen,
     "🐱": CreditScreen,
   },
-  {}
+  {
+    initialRouteName: "Play",
+    tabBarOptions: {
+      activeTintColor: "#f4baba",
+      labelStyle: {
+        fontSize: 16,
+      },
+      inactiveTintColor: "#f4baba",
+    },
+    defaultNavigationOptions: () => ({
+      tabBarOnPress: ({ navigation, defaultHandler }) => {
+        if (navigation.state.key === "Play") {
+          navigation.navigate("Play");
+        } else {
+          defaultHandler();
+        }
+      },
+    }),
+  }
 );
 
 export default createAppContainer(AppNavigator);
